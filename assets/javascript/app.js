df1f1fbcd167c985ac9e33527c688377bd33cbf7
Justin Carlson <justin.ryan.carlson@gmail.com>
2016-11-15 19:29:53 -0600

    adds query strings for the khan, youtube, and NASA APIs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,4 +10,7 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 var searchTerm = "";
-var courseraQuery = "https://api.coursera.org/api/courses.v1?q=search&query=" + searchTerm + "&fields=photoUrl,description";
+var courseraQuery = "https://api.coursera.org/api/courses.v1?q=search&query=" + searchTerm + "&fields=photoUrl,description,name";
+var youtubeQuery = "https: //www.googleapis.com/youtube/v3/search?part=snippet&q=" + searchTerm + "&maxResults=15&order=relevance&key=AIzaSyBInOsaJzC5bjqZp0eHlS3V7vM3QNkhwVg";
+var nasaQuery = 'https://api.nasa.gov/planetary/sounds?q=' + searchTerm + '&api_key=nzTA0ARjHxwopkNivUZKVDzWOfdQciYlgIbEV4XW';
+var youtubeWatchURL = "https://www.youtube.com/watch?v=";
