2aebe36721abed6ff5d3c61ba7a9df092a9082f9
Justin Carlson <justin.ryan.carlson@gmail.com>
2016-11-16 19:13:55 -0600

    updates index.html for testing, adds onclick for the search bars submit button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,7 @@
+//
+// FIREBASE INITIALIZE ==================================================================================
+//
+
 var config = {
     apiKey: "AIzaSyA0WvCTQpp042R62Jtz3Gy_qXbDnNm97XE",
     authDomain: "planning-for-differentiation.firebaseapp.com",
@@ -8,9 +12,53 @@ var config = {
 firebase.initializeApp(config);
 
 
+//
+// GLOBAL VARIABLES ==================================================================================
+//
+
 var database = firebase.database();
-var searchTerm = "";
-var courseraQuery = "https://api.coursera.org/api/courses.v1?q=search&query=" + searchTerm + "&fields=photoUrl,description,name";
-var youtubeQuery = "https: //www.googleapis.com/youtube/v3/search?part=snippet&q=" + searchTerm + "&maxResults=15&order=relevance&key=AIzaSyBInOsaJzC5bjqZp0eHlS3V7vM3QNkhwVg";
-var nasaQuery = 'https://api.nasa.gov/planetary/sounds?q=' + searchTerm + '&api_key=nzTA0ARjHxwopkNivUZKVDzWOfdQciYlgIbEV4XW';
 var youtubeWatchURL = "https://www.youtube.com/watch?v=";
+
+
+//
+// CODE ==================================================================================
+//
+
+
+
+$("#submit-button").on('click', function() {
+    var searchTerm = $('#search-box').val().trim();
+    $('#search-box').val('');
+    console.log(searchTerm);
+    youtubeAPIRequest();
+    // // youtubeKhanAPIRequest();
+    // nasaAPIRequest();
+    // courseraAPIRequest();
+
+
+
+    return false;
+});
+
+
+//
+// FUNCTIONS ==================================================================================
+//
+
+
+function youtubeAPIRequest() {
+    var youtubeQuery = "https: //www.googleapis.com/youtube/v3/search?part=snippet&q=" + searchTerm + "&maxResults=15&order=relevance&key=AIzaSyBInOsaJzC5bjqZp0eHlS3V7vM3QNkhwVg";
+    cnosole.log(youtubeQuery);
+}
+
+function youtubeKhanAPIRequest() {
+
+}
+
+function nasaAPIRequest() {
+    var nasaQuery = 'https://api.nasa.gov/planetary/sounds?q=' + searchTerm + '&api_key=nzTA0ARjHxwopkNivUZKVDzWOfdQciYlgIbEV4XW';
+}
+
+function courseraAPIRequest() {
+    var courseraQuery = "https://api.coursera.org/api/courses.v1?q=search&query=" + searchTerm + "&fields=photoUrl,description,name";
+}
