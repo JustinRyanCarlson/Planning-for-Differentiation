a2c12e849b2106bdb2532fd05d5f6483f3c69b04
Justin Carlson <justin.ryan.carlson@gmail.com>
2016-11-16 20:40:49 -0600

    addes a AJAX call to the youtube function that runs when a search term is submitted, after the call has been returned the function places the img, title, and description into a div and appends it to the id=youtube element

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,12 +30,9 @@ $("#submit-button").on('click', function() {
     var searchTerm = $('#search-box').val().trim();
     $('#search-box').val('');
     console.log(searchTerm);
-    youtubeAPIRequest();
-    // // youtubeKhanAPIRequest();
-    // nasaAPIRequest();
-    // courseraAPIRequest();
-
-
+    youtubeAPIRequest(searchTerm);
+    youtubeKhanAPIRequest(searchTerm);
+    courseraAPIRequest(searchTerm);
 
     return false;
 });
@@ -46,19 +43,30 @@ $("#submit-button").on('click', function() {
 //
 
 
-function youtubeAPIRequest() {
-    var youtubeQuery = "https: //www.googleapis.com/youtube/v3/search?part=snippet&q=" + searchTerm + "&maxResults=15&order=relevance&key=AIzaSyBInOsaJzC5bjqZp0eHlS3V7vM3QNkhwVg";
-    cnosole.log(youtubeQuery);
+function youtubeAPIRequest(searchTerm) {
+    var youtubeQuery = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + searchTerm + "&maxResults=15&order=relevance&key=AIzaSyBInOsaJzC5bjqZp0eHlS3V7vM3QNkhwVg";
+    console.log(youtubeQuery);
+    $.ajax({ url: youtubeQuery, method: 'GET' }).done(function(response) {
+        console.log(response);
+        for (var i = 0; i < 10; i++) {
+            var newEntry = $('<div>');
+            var description = response.items[i].snippet.description;
+            var img = $('<img>');
+            img.attr('src', response.items[i].snippet.thumbnails.medium.url);
+            var title = response.items[i].snippet.title;
+            newEntry.append(img).append(title).append(description);
+            $('#youtube').append(newEntry);
+        }
+    });
 }
 
-function youtubeKhanAPIRequest() {
-
+function youtubeKhanAPIRequest(searchTerm) {
+    var khanYoutubeQuery = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + searchTerm + "&maxResults=15&channelId=UC4a-Gbdw7vOaccHmFo40b9g&order=relevance&key=AIzaSyBInOsaJzC5bjqZp0eHlS3V7vM3QNkhwVg";
+    console.log(khanYoutubeQuery);
 }
 
-function nasaAPIRequest() {
-    var nasaQuery = 'https://api.nasa.gov/planetary/sounds?q=' + searchTerm + '&api_key=nzTA0ARjHxwopkNivUZKVDzWOfdQciYlgIbEV4XW';
-}
 
-function courseraAPIRequest() {
+function courseraAPIRequest(searchTerm) {
     var courseraQuery = "https://api.coursera.org/api/courses.v1?q=search&query=" + searchTerm + "&fields=photoUrl,description,name";
+    console.log(courseraQuery);
 }
